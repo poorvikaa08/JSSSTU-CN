40adef5902b65faf14f004300c00bd44fd4d7fd4
poorvikaa08 <poorvikaa08@gmail.com>
2026-01-02 04:07:14 +0000

    udp

---
+++ UDP/Client.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+class Client {
+	public static void main(String[] args) throws Exception {
+		DatagramSocket clientSocket = new DatagramSocket();
+                BufferedReader inFormUser = new BufferedReader(new InputStreamReader(System.in));
+                
+                InetAddress IPAddress = InetAddress.getByName("localhost");
+                
+                byte[] sendData = new byte[1024];
+                byte[] receiveData = new byte[1024];
+                
+                System.out.println("Enter the string in lowercase so that you recieve the message in Uppercase from the server");
+                
+                String sentence = inFormUser.readLine();
+                sendData = sentence.getBytes();
+                
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 5454);
+                clientSocket.send(sendPacket);
+                
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
+                
+                String modifiedSentence = new String(receivePacket.getData());
+                System.out.println("FROM SERVER: "+ modifiedSentence);
+                
+                clientSocket.close();
+        }
+}
---
+++ UDP/Server.java
@@ -0,0 +1,28 @@
+import java.net.*;
+
+class Server { 
+        public static void main(String args[]) throws Exception {
+                DatagramSocket serverSocket = new DatagramSocket(5454);
+                System.out.println("Server is Ready for the client");
+                
+                byte[] recieveData = new byte[1024];
+                byte[] sendData = new byte[1024];
+                
+                while (true){
+                        DatagramPacket recievePacket = new DatagramPacket(recieveData, recieveData.length);
+                        serverSocket.receive(recievePacket);
+                        
+                        String sentence = new String(recievePacket.getData());
+                        System.out.println("RECEIVED: " + sentence);
+                        
+                        InetAddress IPAddress = recievePacket.getAddress();
+                        int port = recievePacket.getPort();
+                        
+                        String capitalized = sentence.toUpperCase();
+                        sendData = capitalized.getBytes();
+                        
+                        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                        serverSocket.send(sendPacket);
+                }
+        }
+}
